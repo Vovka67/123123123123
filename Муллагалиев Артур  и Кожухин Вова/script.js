ab8795b8f8b970287fa9e1ee6bc358ee018ac4db
Vovka67 <114399957+Vovka67@users.noreply.github.com>
2023-01-17 15:47:48 +0500

    Add files via upload

---
+++ Муллагалиев Артур  и Кожухин Вова/script.js
@@ -0,0 +1,58 @@
+/*let min = 58;
+if(min > 0 && min < 13){
+    console.log('В первую')
+}
+else if(min > 14 && min < 26){
+    console.log('Во вторую')
+}
+else if(min > 27 && min < 43){
+    console.log('В третью')
+}
+else if(min > 44 && min < 59){
+    console.log('В четвертую')
+}*/
+
+/*lang = 'ru';
+if(lang == 'ru'){
+    arr = ['пн','вт','ср','чт','пт','сб','вс'];
+}
+else if(lang == 'en'){
+    arr = ['mn','ts','wd','th','fr','st','sn'];
+}
+console.log(arr);*/
+
+/*let red = "да", yellow = "нет";
+if (red == "да"){
+console.log("Красный сигнал");
+}
+else if(yellow == "да"){
+console.log("Желтый сигнал");
+}
+else{
+console.log("Зелёный сигнал можно переходить дорогу.");
+}*/
+
+/*let i = 1;
+while(i <= 12){
+    console.log(i);
+    i++;
+}*/
+
+/*let i = 5;
+while(i <= 13){
+    console.log(i);
+    i++;
+}*/
+
+/*for (let i = 0; i <= 16; i++) {
+    if(i % 2 == 0) {
+        console.log(i)
+    }
+    }*/
+
+let result8=0;
+let arr8=[2,5,8,3,6];
+for(let i8=0; i8<arr8.length; i8++){
+    result8=result8+arr8[i8];
+}
+console.log(result8);
